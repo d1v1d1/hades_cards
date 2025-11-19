f6748086543727f338ada12457f231457c6d06f8
dividi <dleviavant@gmail.com>
2025-11-19 21:03:09 +0100

    add background plane, mitigate same player names, better scores display, border on mouse over

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1763499775|15848100310';
+const CACHE_VERSION = '1763582531|14091013502';
 /** @type {string} */
 const CACHE_PREFIX = 'hades_cards-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
