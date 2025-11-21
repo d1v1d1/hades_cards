57415729b6dfb431e64d139730e64ce88d43f598
dividi <dleviavant@gmail.com>
2025-11-21 17:06:25 +0100

    add text, change card info style

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1763671076|851662078';
+const CACHE_VERSION = '1763741071|1606024996';
 /** @type {string} */
 const CACHE_PREFIX = 'hades_cards-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
