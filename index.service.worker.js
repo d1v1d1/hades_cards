7d77c02175f9046c3c5679c2dd6331af777cfa1e
dividi <dleviavant@gmail.com>
2025-11-21 17:54:57 +0100

    add/remove some characters

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1763741071|1606024996';
+const CACHE_VERSION = '1763744078|4613130655';
 /** @type {string} */
 const CACHE_PREFIX = 'hades_cards-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
