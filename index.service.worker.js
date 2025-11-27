15b167c3ee8cfeaa64967fada1a6455871674ee0
dividi <dleviavant@gmail.com>
2025-11-27 15:29:14 +0100

    Second flipped card is always above the first one

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1764252504|1859981448';
+const CACHE_VERSION = '1764253705|3061286123';
 /** @type {string} */
 const CACHE_PREFIX = 'hades_cards-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
