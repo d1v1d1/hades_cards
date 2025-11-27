f8abaa0ce3659d9b28be89ef580af3e26f0d6b30
dividi <dleviavant@gmail.com>
2025-11-27 15:09:09 +0100

    slow down card flip on loose. Zoom on card when flipped

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1764186041|5552283824';
+const CACHE_VERSION = '1764252504|1859981448';
 /** @type {string} */
 const CACHE_PREFIX = 'hades_cards-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
