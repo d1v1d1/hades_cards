b8d5cf2790e24b63cdb442c36ff0fd9d5ce65ebd
dividi <dleviavant@gmail.com>
2025-11-28 17:56:04 +0100

    add link to wikipedia

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1764272446|21801251024';
+const CACHE_VERSION = '1764348935|1250713494';
 /** @type {string} */
 const CACHE_PREFIX = 'hades_cards-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
