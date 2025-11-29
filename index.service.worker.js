762ccde5b81a1e53700937e9c8e0cdd831ebfe2e
dividi <dleviavant@gmail.com>
2025-11-29 15:00:47 +0100

    set resolution to 16:9

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1764348935|1250713494';
+const CACHE_VERSION = '1764424782|1471305906';
 /** @type {string} */
 const CACHE_PREFIX = 'hades_cards-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
