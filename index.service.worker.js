b7dc9aadd4501128e7df4089bf218f74f32f86b8
dividi <dleviavant@gmail.com>
2025-12-18 16:11:11 +0100

    add many cards in families

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1766055833|7658892489';
+const CACHE_VERSION = '1766070603|22428994843';
 /** @type {string} */
 const CACHE_PREFIX = 'hades_cards-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
