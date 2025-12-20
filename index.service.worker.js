0bbc75a394a75006e5be43dfd6b3b5d4d9bf4062
dividi <dleviavant@gmail.com>
2025-12-20 15:36:48 +0100

    add missing cards when a filter is used

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1766076054|27880253690';
+const CACHE_VERSION = '1766241358|16615776799';
 /** @type {string} */
 const CACHE_PREFIX = 'hades_cards-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
