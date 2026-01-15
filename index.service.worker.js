cf2ef7b38f7ad3e6a9943c2d6d5a3ec469e2fc84
dividi <dleviavant@gmail.com>
2026-01-15 18:47:55 +0100

    change loading screen

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1768497109|3351037635';
+const CACHE_VERSION = '1768499260|5501898010';
 /** @type {string} */
 const CACHE_PREFIX = 'hades_cards-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
