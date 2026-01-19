06e00fe02d28e2afece1a14556bc7f2b4e9a5ebd
dividi <dleviavant@gmail.com>
2026-01-19 20:53:59 +0100

    add Dionysos special card

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1768848019|2787295792';
+const CACHE_VERSION = '1768852409|7177495142';
 /** @type {string} */
 const CACHE_PREFIX = 'hades_cards-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
