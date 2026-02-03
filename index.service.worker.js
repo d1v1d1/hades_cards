04e3c4213c6f3ae99f5615dabf6817c1b31e1a40
dividi <dleviavant@gmail.com>
2026-02-03 18:29:32 +0100

    toggle dio and hermes special cards

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1768852409|7177495142';
+const CACHE_VERSION = '1770139679|13125358';
 /** @type {string} */
 const CACHE_PREFIX = 'hades_cards-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
