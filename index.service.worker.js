a43612a845472052cc853d50605fc10aca22d4fc
dividi <dleviavant@gmail.com>
2026-02-20 17:57:23 +0100

    try to organize the menu, delete some filter option

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1771599181|11387182153';
+const CACHE_VERSION = '1771606593|18799469937';
 /** @type {string} */
 const CACHE_PREFIX = 'hades_cards-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
