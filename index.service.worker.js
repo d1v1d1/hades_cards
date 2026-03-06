f0805ae531ccc63747e156aa973ad7c056eb6453
dividi <dleviavant@gmail.com>
2026-03-06 17:50:46 +0100

    change music and back for memory

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1771606593|18799469937';
+const CACHE_VERSION = '1772815795|1087409604';
 /** @type {string} */
 const CACHE_PREFIX = 'hades_cards-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
